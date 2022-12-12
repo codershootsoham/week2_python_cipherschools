77f6d7189f5bdb46d789cbf97631986242463af8
codershootsoham <119524002+codershootsoham@users.noreply.github.com>
2022-12-12 21:57:45 +0530

    Add files via upload

---
+++ work20.py
@@ -0,0 +1,12 @@
+# update method
+
+user_info={
+    'name':'raj',
+    'age': 34,
+    'fav_movies': ['3 IDIOTS'],
+    'fav_tunes': ['awakening','fairy tale'],
+}
+
+more_info= {'state' : 'bihar', 'hobbies' : ['coding','studying','singing']}
+user_info.update({})
+print(user_info)
---
+++ work22.py
@@ -0,0 +1,4 @@
+# more about get method
+user= {'name' : 'raj', 'age' : '34', 'age' : '44'}
+# print(user.get('names','not found'))
+print(user)
